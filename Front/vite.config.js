2c519c83a435109a6542f5d5be47de83e69aa418
Alexandre Juliao <alexandre.juliao@grupoepea.com.br>
2025-05-25 11:59:40 -0300

    change ports

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
-    port: process.env.FRONT_PORT || 3042,
+    port: process.env.FRONT_PORT || 3043,
     host: '0.0.0.0',
     allowedHosts: ['transcendence.fazai.dev']
   }
