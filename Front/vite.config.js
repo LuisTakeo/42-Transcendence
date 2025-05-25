9e113b8f484d7e4c4e93d06594f135d9495c4818
Alexandre Juliao <alexandre.juliao@grupoepea.com.br>
2025-05-25 12:01:05 -0300

    add allowhost

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   server: {
     port: process.env.FRONT_PORT || 3043,
     host: '0.0.0.0',
-    allowedHosts: ['transcendence.fazai.dev']
+    allowedHosts: ['transcendence.fazai.dev', 'dev-transc.fazai.dev']
   }
 });
