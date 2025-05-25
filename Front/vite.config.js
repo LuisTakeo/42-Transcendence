7f206b2ea8ecf348eb46da90fa2c5ce6f6db650a
Alexandre Juliao <alexandre.juliao@grupoepea.com.br>
2025-05-25 12:01:48 -0300

    add allowhost pong

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,6 +4,6 @@ export default defineConfig({
   server: {
     port: process.env.FRONT_PORT || 3043,
     host: '0.0.0.0',
-    allowedHosts: ['transcendence.fazai.dev', 'dev-transc.fazai.dev']
+    allowedHosts: ['pong.fazai.dev', 'dev-pong.fazai.dev']
   }
 });
