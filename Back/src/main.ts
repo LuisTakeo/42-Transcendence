c478aafa311c73600d6716caf068c9c1d192d689
BiancaSantana81 <biancasantana81@hotmail.com>
2025-05-27 18:03:08 -0300

    WIP #20 - começando configuração de tokens com fastify/JWT

--- Back/src/main.ts
+++ Back/src/main.ts
@@ -1,14 +1,39 @@
 import fastify from 'fastify';
+import fastifyJwt from 'fastify-jwt';
+import type { FastifyRequest, FastifyReply } from 'fastify';
 
 const server = fastify({ logger: true });
 const port = process.env.BACK_PORT;
 const host = '0.0.0.0';
 
 
+// registra plugin JWT
+
+server.register(fastifyJwt, {
+	secret : process.env.JWT_SECRET || 'supersecret'
+});
+
+// cria middleware para verificar o token JWT
+
+server.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
+  try {
+    await request.jwtVerify();
+  } catch (err) {
+    reply.send(err);
+  }
+});
+
+
 // Define a simple route
-server.get('/', async (request, reply) => {
+server.get('/', async (request: FastifyRequest, reply: FastifyReply) => {
 	return { hello: 'world' };
   });
+
+// Exemplo de rota protegida
+
+server.get('/profile', {preValidation: [server.authenticate]}, async (request: FastifyRequest, reply: FastifyReply) => {
+	return { user: request.user };
+});
 
 const start = async () =>
 {
