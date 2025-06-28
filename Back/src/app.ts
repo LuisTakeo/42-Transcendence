321e1a5243c1cda5973413ba7dbbe115b4d92c3e
Maria Vitoria Vital <mavitori@c2r3p1.42sp.org.br>
2025-06-28 14:49:50 -0300

    backpor

--- Back/src/app.ts
+++ Back/src/app.ts
@@ -12,7 +12,7 @@ import { tournamentRoutes } from './tournaments/tournaments.routes';
 
 export const startServer = async () => {
 	const app = fastify({ logger: true });
-	const port = process.env.BACK_PORT ? Number(process.env.BACK_PORT) : 3143;
+	const port = process.env.BACK_PORT;
 	const host = '0.0.0.0';
 
 	// Habilitar CORS para seu frontend
