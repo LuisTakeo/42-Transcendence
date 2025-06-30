f7c9d016dd3eb51b173bc792ef1fffcb49bee628
vitoriagys <vitoria@agys.com.br>
2025-06-30 20:40:58 -0300

    fix tests

--- Back/src/routes/users/users.routes.ts
+++ Back/src/routes/users/users.routes.ts
@@ -1,7 +1,21 @@
 // users.routes.ts
 import { FastifyInstance } from 'fastify';
-import { getAllUsers } from './users.controller';
+import {
+  getAllUsers,
+  getAllUsersSimple,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  updateUserOnlineStatus
+} from './users.controller';
 
 export default async function usersRoutes(app: FastifyInstance) {
-  app.get('/users', getAllUsers);
+  app.get('/', getAllUsers);
+  app.get('/all', getAllUsersSimple);
+  app.get('/:id', getUserById);
+  app.post('/', createUser);
+  app.put('/:id', updateUser);
+  app.delete('/:id', deleteUser);
+  app.patch('/:id/online-status', updateUserOnlineStatus);
 }
