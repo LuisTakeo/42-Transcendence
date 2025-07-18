84670dc4f33521659f42ed09e85c01fe26357a1a
bda-mota <bea.mota04@gmail.com>
2025-07-17 21:48:45 -0300

    fix how to play

--- Front/src/pages/howToPlay.ts
+++ Front/src/pages/howToPlay.ts
@@ -5,40 +5,40 @@ export default function HowToPlay(): void {
 	app.innerHTML = ""; // Clear existing content
 
 	const main = document.createElement("main");
-	main.className = "min-h-screen flex flex-col justify-center items-center  overflow-hidden mt-4 gap-4";
+	main.className = "min-h-screen flex flex-col justify-center items-center mt-0 gap-2";
 	main.innerHTML =`
-	<div class="flex flex-wrap justify-center gap-8 items-stretch max-w-6xl w-full px-6 py-10">
+	<div class="flex flex-wrap justify-center gap-10 items-stretch max-w-7xl w-full px-4 py-10 mx-auto">
 		<!-- PLAYER 1 -->
-		<div class="flex-1 min-w-[280px] max-w-[400px] bg-[#1E1B4B] p-8 rounded text-white text-center flex flex-col justify-center">
-			<p class="text-4xl md:text-5xl mb-4">Player on the left</p>
-			<p class="text-3xl md:text-3xl flex items-center justify-center gap-2 flex-wrap">
-				Use the keys 
-				<span class="key">W</span>
-				and 
-				<span class="key">S</span>
-				to move.
+		<div class="flex-1 min-w-[320px] max-w-[480px] bg-[#1E1B4B] p-10 rounded-2xl text-white  w-full text-center flex flex-col items-center gap-6">
+			<img src="../../assets/perfil-1.png" class="max-w-full mb-4" />
+			<p class="text-4xl font-semibold md:text-5xl">Player on the left</p>
+			<p class="text-2xl md:text-3xl flex items-center justify-center gap-2 flex-wrap">
+			Use the keys 
+			<span class="key">W</span>
+			and 
+			<span class="key">S</span>
+			to move.
 			</p>
 		</div>
 
 		<!-- PLAYER 2 -->
-		<div class="flex-1 min-w-[280px] max-w-[400px] bg-[#1E1B4B] p-8 rounded text-white text-center flex flex-col justify-center">
-			<p class="text-4xl md:text-5xl mb-4">Player on the right</p>
-			<p class="text-3xl md:text-3xl flex items-center justify-center gap-2 flex-wrap">
-				Use the arrows 
-				<span class="key">↑</span>
-				and 
-				<span class="key">↓</span>
-				to move.
+		<div class="flex-1 min-w-[320px] max-w-[480px] bg-[#1E1B4B] p-10 rounded-2xl text-white text-center flex flex-col items-center gap-6">
+			<img src="../../assets/perfil-2.png" class="max-w-full mb-4" />
+			<p class="text-4xl font-semibold md:text-5xl">Player on the right</p>
+			<p class="text-2xl md:text-3xl flex items-center justify-center gap-2 flex-wrap">
+			Use the keys 
+			<span class="key">↑</span>
+			and 
+			<span class="key">↓</span>
+			to move.
 			</p>
 		</div>
 	</div>
 
 	<!-- BOTÃO START -->
-	<div class="mt-6 mb-8">
-		<button class="p-6 bg-green-600 hover:bg-green-700 text-white text-2xl md:text-3xl font-bold py-4 px-10 rounded transition">
+		<button class="p-4 mb-12 border-2 border-[#1E1B4B] rounded-xl border-spacing-2 bg-green-600 hover:bg-green-700 text-white text-2xl md:text-3xl font-bold transition">
 			Start
 		</button>
-	</div>
 
 	<style>
 	.key {
@@ -51,9 +51,6 @@ export default function HowToPlay(): void {
 		font-weight: bold;
 		font-family: monospace;
 	} 
-	body {
-		overflow:hidden;
-	}
 	</style>
 
 	`;
